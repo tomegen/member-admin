eb6759bad4dc5eface3a84141d21e3b2ae7ec3bd
Thomas Egen <thomas.egen@bmw.de>
2024-10-01 21:48:57 +0200

    fix problem with customtkinter path

--- build.py
+++ build.py
@@ -3,4 +3,6 @@ import customtkinter
 
 if __name__ == '__main__':
     customtkinter_path = os.path.dirname(os.path.abspath(customtkinter.__file__))
-    os.system("pyinstaller --onedir --windowed --icon member.ico --add-data " + customtkinter_path + ";customtkinter/ mitgliederverwaltung.py")
+    customtkinter_path = customtkinter_path[:customtkinter_path.rfind('/')]
+    print(customtkinter_path)
+    os.system("pyinstaller --onedir --windowed --icon member.ico --add-data=" + customtkinter_path + ":customtkinter/ mitgliederverwaltung.py")
