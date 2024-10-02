54227e3e686386fcbcf06a5a8f4ec7295609b97e
Thomas Egen <thomas.egen@bmw.de>
2024-10-02 07:27:40 +0200

    change build script

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ import customtkinter
 
 if __name__ == '__main__':
     customtkinter_path = os.path.dirname(os.path.abspath(customtkinter.__file__))
-    #customtkinter_path = customtkinter_path[:customtkinter_path.rfind('/')]
+    customtkinter_path = customtkinter_path[:customtkinter_path.rfind('/')]
     print(customtkinter_path)
-    os.popen("pyinstaller --onedir --windowed --icon member.ico --add-data=" + customtkinter_path + ":customtkinter/ mitgliederverwaltung.py")
+    os.system("pyinstaller --onedir --windowed --icon member.ico --add-data=" + customtkinter_path + ":customtkinter mitgliederverwaltung.py")
