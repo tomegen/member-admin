b9d8d3fa8deb795bfa240032331a34bb8ab8d7f8
Thomas Egen <thomas.egen@bmw.de>
2024-10-05 11:59:11 +0200

    manage hidden imports for babel.numbers and xlrd

--- build.py
+++ build.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     customtkinter_path = os.path.dirname(os.path.abspath(customtkinter.__file__))
     #customtkinter_path = customtkinter_path[:customtkinter_path.rfind('')]
     print(customtkinter_path)
-    os.system("pyinstaller --onedir --windowed --icon member.ico --add-data=" + customtkinter_path + ":customtkinter/ mitgliederverwaltung.py")
+    os.system("pyinstaller --onedir --windowed --icon member.ico --hidden-import babel.numbers --hidden-import xlrd --add-data=" + customtkinter_path + ":customtkinter/ mitgliederverwaltung.py")
